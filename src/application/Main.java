82b8c3606d543dc4524b6ed3ee700562e3428f60
소상훈 <sshlc940503@naver.com>
2019-01-27 16:20:53 +0900

    첫 커밋

---
+++ src/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		Parent root =FXMLLoader.load(getClass().getResource("Cal.fxml"));
+		primaryStage.setScene(new Scene(root));
+		primaryStage.setTitle("Calculator");
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
